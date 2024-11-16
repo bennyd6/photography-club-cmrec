744519abd9fb1f4c4495856238c7d6bf0b7a5f06
bennyd6 <228r1a66d6@cmrec.ac.in>
2024-11-16 18:39:55 +0530

    added some style

--- src/components/achievements.jsx
+++ src/components/achievements.jsx
@@ -1,23 +1,33 @@
 import './achievements.css';
 import Carousal from './carousal';
-import aw1 from '../assets/award.jpg';
-import aw2 from '../assets/1-1.jpg';
-import aw3 from '../assets/1-2.jpg';
-import aw4 from '../assets/1-3.jpg';
+import im1 from '../assets/1-1.jpg';
+import im2 from '../assets/1-2.jpg';
+import im3 from '../assets/1-3.jpg';
+import im4 from '../assets/2-1.jpg';
+import im5 from '../assets/2-2.jpg';
+import im6 from '../assets/2-3.jpg';
+import im7 from '../assets/3-1.jpg';
+import im8 from '../assets/3-2.jpg';
+import im9 from '../assets/3-3.jpg';
 
 export default function Achievements() {
     const carouselData1 = {
-        images: [aw1, aw2, aw3, aw4],
-        heading: "IIT Kharagpur Achievements",
+        images: [im1, im2, im3],
+        heading: "Short Film Making (IIT Kharagpur)",
         paragraph: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia ullam doloribus laborum sit, maiores impedit beatae vero optio quam, similique non nisi cupiditate odit sunt, illum ratione."
     };
 
     const carouselData2 = {
-        images: [aw2, aw3, aw4, aw1],
-        heading: "Innovation at IIT Kharagpur",
+        images: [im4, im5, im6],
+        heading: "Motion Tales (IIT Kharagpur)",
         paragraph: "Natus, perferendis ipsum qui consequatur. Vero, aliquam pariatur. Distinctio sit rem mollitia dolores. Tempore corporis ullam exercitationem perferendis!"
     };
-
+    
+    const carouselData3={
+        images: [im7, im8, im9],
+        heading: "Cinevision 2k24 (CMRTC)",
+        paragraph: "Natus, perferendis ipsum qui consequatur. Vero, aliquam pariatur. Distinctio sit rem mollitia dolores. Tempore corporis ullam exercitationem perferendis!"
+    }
     return (
         <div className="achievements-main">
             <div className="achievements-heading">
@@ -33,6 +43,11 @@ export default function Achievements() {
                     images={carouselData2.images} 
                     heading={carouselData2.heading} 
                     paragraph={carouselData2.paragraph} 
+                />
+                <Carousal 
+                    images={carouselData3.images} 
+                    heading={carouselData3.heading} 
+                    paragraph={carouselData3.paragraph} 
                 />
             </div>
         </div>
