d6695fe30a590f5c5a7dfbc87453ffa03b0585cd
bennyd6 <228r1a66d6@cmrec.ac.in>
2024-11-20 01:08:05 +0530

    added contact page

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -1,7 +1,20 @@
+import './contact.css'
+
 export default function Contact(){
+    function onSubmit(){
+        console.log("hi");
+    }
     return(
         <>
-        <div>hi</div>
+        <div className="contact-main">
+            <h1>Ping us here!</h1>
+            <form onSubmit={onSubmit}>
+                <input type="text" name="name" required placeholder="Enter your name" />
+                <input type="email" name="email" placeholder="Enter your email" required />
+                <textarea name="message" id="msg" required placeholder="Leave your message here"></textarea>
+                <button type="submit">Submit Form</button>
+            </form>
+        </div>
         </>
     )
 }
